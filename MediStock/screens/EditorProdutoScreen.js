24e1505361e443d44548ea432afa68951ebbc9d5
lucascarvalholopes <lucascarvalholopes2@gmail.com>
2023-10-20 20:21:39 -0300

    atualização do HomeScreen.js

--- MediStock/screens/EditorProdutoScreen.js
+++ MediStock/screens/EditorProdutoScreen.js
@@ -1,20 +1,73 @@
-import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, Modal,Image } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import { Camera } from "expo-camera";
 
-function EditorProdutoScreen() {
-  const handleCameraPress = () => {
-    // Implemente a lógica para abrir a câmera aqui
-    // Por exemplo, você pode usar a biblioteca react-native-camera
-    // e navegar para a tela da câmera
+const EditorProdutoScreen = () => {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [photo, setPhoto] = useState(null);
+  const [cameraVisible, setCameraVisible] = useState(false);
+
+  const cameraRef = useRef(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  const handleCapture = async () => {
+    if (cameraRef.current) {
+      const { uri } = await cameraRef.current.takePictureAsync();
+      console.log("Photo taken:", uri);
+      setPhoto(uri);
+      setCameraVisible(false);
+    }
   };
+
 
   return (
     <View style={styles.container}>
       <Text>Editor de Produto Screen</Text>
-      <TouchableOpacity onPress={handleCameraPress} style={styles.cameraButton}>
-        <Icon name="camera" size={50} color="white" />
+      <TouchableOpacity onPress={() => setCameraVisible(true)} style={styles.cameraButton}>
+        {photo ? (
+          <Image source={{ uri: photo }} style={styles.profileImage} />
+        ) : (
+          <View style={styles.iconContainer}>
+            <Text>
+              <Icon name="camera" size={50} color="white" />
+            </Text>
+          </View>
+        )}
       </TouchableOpacity>
+
+      <View>
+        <Modal
+          animationType="slide"
+          transparent={false}
+          visible={cameraVisible}
+          onRequestClose={() => setCameraVisible(false)}
+        >
+          <Camera
+            ref={cameraRef}
+            style={styles.camera}
+            type={Camera.Constants.Type.back}
+            ratio="16:9"
+          >
+            <TouchableOpacity
+              style={styles.captureButton}
+              onPress={() => handleCapture()}
+            >
+              <View style={styles.captureButtonInner}>
+                <View style={styles.captureButtonCircle}>
+                  <View style={styles.captureButtonCenter} />
+                </View>
+              </View>
+            </TouchableOpacity>
+          </Camera>
+        </Modal>
+      </View>
     </View>
   );
 }
@@ -30,6 +83,37 @@ const styles = StyleSheet.create({
     padding: 20,
     borderRadius: 50,
     marginTop: 20,
+  },
+  camera: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  captureButton: {
+    alignSelf: "center",
+    marginBottom: 20,
+  },
+  captureButtonInner: {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+    backgroundColor: "transparent",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  captureButtonCircle: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    backgroundColor: "lightgray",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  captureButtonCenter: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    backgroundColor: "gray",
   },
 });
 
--- MediStock/screens/HomeScreen.js
+++ MediStock/screens/HomeScreen.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Platform } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+
+const plataforma = Platform.OS;
 
 function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
+    <View style={{ flex: 1 }}>
+      <View style={{ flex: 1, backgroundColor: 'blue', justifyContent: "center", alignItems: "center"}}>
         <Text style={styles.headerText}>MediStock</Text>
       </View>
-
-      <View style={styles.content}>
+      <View style={{ flex: 8, flexDirection: plataforma === "web" ? "row" : "column", justifyContent: "center", alignItems: "center", flexWrap: plataforma === "web" ? "wrap" : undefined, gap: 10, padding: 20 }}>
         <TouchableOpacity
           style={styles.itemContainer}
           onPress={() => navigation.navigate('Estoque')}
@@ -19,7 +20,6 @@ function HomeScreen({ navigation }) {
             <Text style={styles.itemText}>Estoque de Medicamentos</Text>
           </View>
         </TouchableOpacity>
-
         <TouchableOpacity
           style={styles.itemContainer}
           onPress={() => navigation.navigate('EditorProduto')}
@@ -29,19 +29,17 @@ function HomeScreen({ navigation }) {
             <Text style={styles.itemText}>Editor de Produto</Text>
           </View>
         </TouchableOpacity>
-
         <TouchableOpacity
           style={styles.itemContainer}
           onPress={() => navigation.navigate('Pedido')}
         >
           <View style={[styles.square, styles.darkGreenBackground]}>
-            <Text style={styles.itemText}>Pedido</Text>
             <Ionicons name="cart" size={64} color="white" />
+            <Text style={styles.itemText}>Pedido</Text>
           </View>
         </TouchableOpacity>
       </View>
-
-      <View style={styles.footer}>
+      <View style={{ flex: 1, backgroundColor: 'blue', justifyContent: "center", alignItems: "center" }}>
         <Text style={styles.footerText}>MediStock</Text>
       </View>
     </View>
@@ -72,8 +70,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   itemContainer: {
-    alignItems: 'center',
-    margin: 10,
+    // alignSelf: "center",
+    // alignItems: 'center',
   },
   square: {
     width: 160,
@@ -107,4 +105,3 @@ const styles = StyleSheet.create({
 });
 
 export default HomeScreen;
-
